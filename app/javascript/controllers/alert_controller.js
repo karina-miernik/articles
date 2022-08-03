1c1dcb3f14f388e7f860c4ba2ccff00e9fd18bc8
Karina Miernik <karina.miernik@gmail.com>
2022-08-03 11:28:50 +0200

    add targets and values to stimulus controller

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,8 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="alert"
 export default class extends Controller {
-
+  static targets = ["btn"]
+  static values = {
+    text: String
+  }
   connect() {
+    console.log('connected')
+    console.log(this.btnTarget)
+    console.log(this.textValue)
   }
   close() {
     this.element.remove()
